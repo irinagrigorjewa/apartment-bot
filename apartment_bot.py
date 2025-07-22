43a030dabb98fc3b1af1ac446affe01159b0bed0
Irina Grigoryeva <irkagrigorjewa@gmail.com>
2025-07-22 13:34:28 +0300

    fix: change environment variables

--- apartment_bot.py
+++ apartment_bot.py
@@ -6,9 +6,9 @@ import schedule
 import time
 import threading
 
-# 🔑 Токен и твой Telegram ID
-TELEGRAM_TOKEN = '7600421133:AAHGaWINYagwEyS2WzEfaowsbreFvoZ9rWQ'
-YOUR_CHAT_ID = '603000026'
+# 🔑 Токен и Telegram ID
+TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
+YOUR_CHAT_ID = os.environ['YOUR_CHAT_ID']
 MAX_PRICE = 360
 
 bot = Bot(token=TELEGRAM_TOKEN)
